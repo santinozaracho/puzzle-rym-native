bf0362badb6404c510979d9a3e25896d4568d251
Carlos Santino Zaracho Simonetto <carlosszaracho@gmail.com>
2020-10-12 16:35:27 -0300

    disable Sentry

--- App.tsx
+++ App.tsx
@@ -11,7 +11,6 @@
 import React, { useEffect, PureComponent } from 'react';
 import { SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar } from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
-import * as Sentry from '@sentry/react-native';
 import VersionNumber from 'react-native-version-number';
 import { ThemeProvider } from './src/store/ThemeContext';
 import { Provider } from './src/store/GeneralContext';
@@ -24,31 +23,8 @@ import config, { isProduction } from './config';
 
 class App extends PureComponent {
   componentDidMount() {
-    this.initSentry();
     SplashScreen.hide();
   }
-
-  initSentry = () => {
-    let sentryEnvironment = '';
-    if (!isProduction && !__DEV__) {
-      sentryEnvironment = 'staging';
-    }
-    if (isProduction && !__DEV__) {
-      sentryEnvironment = 'production';
-    }
-    if (__DEV__) {
-      sentryEnvironment = 'development';
-    }
-    if (!__DEV__) {
-      Sentry.init({
-        dsn: config.SENTRY_DSN,
-      });
-    }
-    Sentry.setTag('environment', `${sentryEnvironment}`);
-    Sentry.setTag('react', 'true');
-    Sentry.setExtra('version', `${VersionNumber.appVersion}`);
-    Sentry.setExtra('environmentAPI', isProduction ? 'production' : 'staging');
-  };
 
   render() {
     const usingHermes = typeof HermesInternal === 'object' && HermesInternal !== null;
